61acb6dcc7e32d3cafe559e45492ad9282ff8374
yellow <yellow@wordncode.com>
2022-12-15 17:28:01 +0900

    게시판 작업 완료

--- resource/js/sub.js
+++ resource/js/sub.js
@@ -1,6 +1,28 @@
 $(function(){
 
-	//
 	$(".contents-sub").prev().addClass("header-sub");
 
+
+	$(".ui-input, .ui-textarea").on("change keyup paste", function() {
+        var currentVal = $(this).val();
+        if(currentVal == "") {
+            $(this).removeClass("is-inputed");
+        } else {
+            $(this).addClass("is-inputed");
+        }
+    });
+
+    $(".ui-select").on("change keyup paste", function() {
+        var result = $('.ui-select option:selected').val();
+		console.log(result);
+        if(result == "") {
+            $(this).removeClass("is-inputed");
+        } else {
+			$(this).addClass("is-inputed");
+            //$(this).removeClass("is-inputed");
+        }
+    });
+
+
 });
+
